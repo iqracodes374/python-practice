12a401b7441b6828461f7ab7ce35967d76ad40a7
iqracodes2026 <da009465@gmail.com>
2026-07-06 19:17:52 +0500

    Day 3: module with pip,pyjokes,emoji,cowsay

---
+++ Day-3-module pip/main.py
@@ -0,0 +1,10 @@
+import pyjokes
+print(pyjokes.get_joke())
+import emoji
+print(emoji.emojize("i love :pizza: :heart:"))
+import cowsay
+cowsay.cow("hello api")
+import datetime
+print(datetime.date.today())
+import random
+print(random.randint(1,100))
