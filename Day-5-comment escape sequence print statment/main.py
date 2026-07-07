7ffc202c3b966987764a8f6011555ee1595dce5c
iqracodes2026 <da009465@gmail.com>
2026-07-07 19:48:28 +0500

    comment escape sequence print statment added

---
+++ Day-5-comment escape sequence print statment/main.py
@@ -0,0 +1,8 @@
+# this code is written by iqra
+print("i love \"biryani\"\nand i also love \'pizza\'")
+'''
+in this code i learn about comment escape sequence print statment
+i am very very happy to learn python
+'''
+print("hello world,hi,bye",sep="-",end="008")
+print("harry")
