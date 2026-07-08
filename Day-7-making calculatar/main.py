573c1da35922140c65699fcc40a9276277c597bc
iqracodes2026 <da009465@gmail.com>
2026-07-08 20:32:43 +0500

    making calculatar added

---
+++ Day-7-making calculatar/main.py
@@ -0,0 +1,9 @@
+num1 = 54
+num2 = 48
+print("result of calculatar")
+print("addition:",num1,"+",num2,"=" ,num1+num2)
+print("subtraction:",num1,"-",num2,"=" ,num1-num2)
+print("multiplication:",num1,"*",num2,"=" ,num1*num2)
+print("division:",num1,"/",num2,"=" ,num1/num2)
+print("modulus:",num1,"%",num2,"=" ,num1%num2)
+print("floar Division:",num1,"//",num2,"=" ,num1//num2)
