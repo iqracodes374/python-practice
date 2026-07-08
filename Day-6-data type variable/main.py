e1e2880f188e292cd0f70a3d2e2be4042059221b
iqracodes2026 <da009465@gmail.com>
2026-07-08 20:01:08 +0500

    data type variable added

---
+++ Day-6-data type variable/main.py
@@ -0,0 +1,16 @@
+a=12345678910
+b="true"
+c="harry"
+d=None
+a1=10987654321
+print(a+a1)
+print("type of a is:" ,type(a))
+print("type of b is:" ,type(b))
+print("type of c is:" ,type(c))
+print("type of d is:" ,type(d))
+list1= [24,64,98,74,26,123]
+tuple1= ["apple","banana","grapes","orange","mango"]
+dict1= {"name":"amina","age":20,"canvote":True}
+print(list1)
+print(tuple1)
+print(dict1)
