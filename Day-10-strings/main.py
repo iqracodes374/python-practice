5f047491cfac34ad76610895e12156256ff1a5eb
iqracodes2026 <da009465@gmail.com>
2026-07-13 12:50:24 +0500

    string added

---
+++ Day-10-strings/main.py
@@ -0,0 +1,18 @@
+name = "danish"
+friend = "abid"
+anotherfriend = "hamid"
+print("hello," + name)
+apple = "he said, i want to eat apple"
+print(apple)
+text = """ hi danish 
+how are you
+are you fine """
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print("lets use for loop\n")
+for character in text:
+    print(character)
