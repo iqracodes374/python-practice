5b24321396e3edb6c2675d7f59c9ed345e54f722
iqracodes2026 <da009465@gmail.com>
2026-07-15 20:01:12 +0500

    method of string added

---
+++ Day-12-string method/main.py
@@ -0,0 +1,29 @@
+# String are immutable
+a = "rehman...."
+print(len(a))
+print(a)
+print(a.upper())
+print(a.lower())
+print(a.rstrip("."))
+print(a.replace("rehman","abid"))
+print(a.split(" "))
+blogheading = "intrcduction is english"
+print(blogheading.capitalize())
+srt1 = "welcome to console!!!"
+print(len(srt1))
+print(len(srt1.center(50)))
+print(a.count("harry"))
+str2 = "welcome to console!!!"
+print(str2.endswith("!!!"))
+str3 = "he's name is dan he is an honest man"
+print(str3.find("is"))
+print(str3.index("honest"))
+str4 = "i am very intelligent"
+print(str4.isalnum())
+print(str4.isalpha())
+print(str4.islower())
+print(str4.isprintable())
+print(str4.isspace())
+print(str4.istitle())
+print(str4.swapcase())
+print(str4.title())
