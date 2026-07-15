42e05fd6b50301c0782646b764a5c207d458f227
iqracodes2026 <da009465@gmail.com>
2026-07-15 19:05:23 +0500

    string slicing added

---
+++ Day-11-string slicing/main.py
@@ -0,0 +1,13 @@
+fruit = "banana"
+bananalen = len(fruit)
+print(bananalen)
+print(fruit[0:5]) #including 0 not 5
+print(fruit[1:5]) #including 1 not 5
+print(fruit[2:5]) #including 2 not 5
+print(fruit[:4])
+print(fruit[0:-3])
+print(fruit[-1:-3])
+print(fruit[-3:-1])
+# quick quiz
+nm ="harry"
+print(nm[-4:-2])
