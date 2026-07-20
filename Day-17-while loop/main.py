b2131233634d1692c3280c045a5cc00a454029c3
iqracodes2026 <da009465@gmail.com>
2026-07-20 17:39:23 +0500

    while loop added

---
+++ Day-17-while loop/main.py
@@ -0,0 +1,11 @@
+i = int(input("enter the number"))
+while(i<=39):
+    i = int(input("enter the number"))
+    print(i)
+print("done with the loop")
+count = 5
+while(count>0):
+    print(count)
+    count = count-1
+else:
+    print("i am inside else function")
