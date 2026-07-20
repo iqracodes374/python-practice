c5f7004f1c7008ff0974237a73b220ed01c8581f
iqracodes2026 <da009465@gmail.com>
2026-07-20 17:10:36 +0500

    for loop added

---
+++ Day-16-for loops/main.py
@@ -0,0 +1,16 @@
+name = "ahmad"
+for i in name:
+    print(i)
+    if(i=="m"):
+        print("this is a third alphabat")
+colors = ("red" , "green" , "yellow" , "orange")
+for color in colors:
+    print(colors)
+for x in colors:
+    print(x)
+for k in range(5):
+    print(k)
+for k in range(1,15,4):
+    print(k+2)
+
+
