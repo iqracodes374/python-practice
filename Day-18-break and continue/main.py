fd78a3c583474093217363c08717f85b00bae7d8
iqracodes2026 <da009465@gmail.com>
2026-07-22 15:47:48 +0500

    break and continue statment added

---
+++ Day-18-break and continue/main.py
@@ -0,0 +1,20 @@
+for i in range(12):
+    print("5 X", i+1, "=" ,5 * (i+1))
+    if(i==10):
+        break
+print("loop ko chor gaya")
+# continue statment
+for i in range(15):
+    print("5 X",i, "=",5*i)
+    if(i==11):
+        print("skip the iteration")
+        continue
+        
+    
+
+
+
+
+
+
+
