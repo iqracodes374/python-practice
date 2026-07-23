81236e491fde9796b69595bf2a591eee71ead1db
iqracodes2026 <da009465@gmail.com>
2026-07-23 18:12:35 +0500

    function in python added

---
+++ Day-20-functions in python/main.py
@@ -0,0 +1,18 @@
+def calculatemean(a,b):
+    mean = (a*b)/(a+b)
+    print(mean)
+def isgreater(a,b):
+    if(a>b):
+        print("first number is greater")
+    else:
+        print("second number is greater")
+def islesser(a,b):
+    pass
+a = 48
+b = 89
+calculatemean(a,b)
+isgreater(a,b)
+c = 12
+d = 41
+calculatemean(c,d)
+isgreater(c,d)
